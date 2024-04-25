6dd5e2ad7c4a7526918d62e13545a6e6691b928b
PRIYTESH PRASAD <61977852+priyteshprasad@users.noreply.github.com>
2024-04-25 12:14:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+let arr = [1,2, 3, 4]
+
+let p1 = new Promise((resolve, reject)=>{
+	setTimeout(()=>{
+		resolve(arr)
+		return arr;
+	}, 3000)
+}).then((arr)=>{
+	arr = arr.filter((item)=>{
+		return item%2 === 0;
+	})
+	return arr;
+}).then((arr)=>{
+	arr = arr.map((item) =>{
+		return item * 2;
+	})
+	return arr;
+}).then((arr)=>{
+	let outElement = document.getElementById('output')
+	outElement.innerHTML = arr; 
+})
